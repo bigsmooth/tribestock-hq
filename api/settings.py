636c4a05ed0de782408c30bf36061f9d9587e8aa
bigsmooth <kevinmronot@gmail.com>
2025-09-22 01:38:32 -0400

    Enable DRF JWT defaults

--- api/settings.py
+++ api/settings.py
@@ -116,3 +116,13 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ),
 }
+
+# DRF + JWT
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
