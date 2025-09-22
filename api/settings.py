357a6f0861d2723fe28fab34d63e4db992a76c21
bigsmooth <kevinmronot@gmail.com>
2025-09-22 01:28:20 -0400

    Enable DRF JWT defaults

--- api/settings.py
+++ api/settings.py
@@ -106,3 +106,13 @@ USE_I18N = True
 
 # Primary key type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# DRF + JWT
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
